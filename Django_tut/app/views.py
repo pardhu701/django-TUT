1e6bf5b45b883540f05d42a09fd1a7aaeb84cbd4
pardhu701 <104940825+pardhu701@users.noreply.github.com>
2025-01-02 00:04:48 +0530

    Add files via upload

--- Django_tut/app/views.py
+++ Django_tut/app/views.py
@@ -16,14 +16,8 @@ def mem(request):
 
     return HttpResponse("pardha  give up")
 
-@csrf_protect
+
 def prac(request):
-    try:
-        if request.method=='POST':
-            email=request.POST['email']
-            password=request.POST['password']
-            return HttpResponse(email)
-    except:
-        pass
+    
     
     return render(request,'prac.html')
